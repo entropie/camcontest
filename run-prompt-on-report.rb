8a53a89e7d5ef454b51ba606007bcfea28818bd9
Michael Trommer <mictro@gmail.com>
2025-09-12 04:01:12 +0200

    run prompt on reports

---
+++ run-prompt-on-report.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require "fileutils"
+
+require_relative "include"
+
+prompt = 
+  if !STDIN.tty? && !STDIN.eof?
+    STDIN.read.strip
+  else
+    File.read(File.join(BASEDIR, "prompt.txt"))
+  end
+
+target_report_directories = ARGV
+
+abort "#{$0} <*report directories>" if target_report_directories.empty?
+
+files = target_report_directories.map { |tdir| Dir.glob("%s/file-*.jpg" % tdir) }.sort.flatten
+
+stdout_str, status = llm_describe_images(prompt, files: files)
+
+# generate report
+rep = Report.new(files, prompt, stdout_str.strip)
+rep.save
+
+puts rep.output_file, stdout_str

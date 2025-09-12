3d973540d43479e774d836eab1e5fd7a7d818a16
Michael Trommer <mictro@gmail.com>
2025-09-12 03:45:53 +0200

    camcontest working with class

--- camcontest.rb
+++ camcontest.rb
@@ -3,25 +3,32 @@
 require_relative "include"
 
 ha = Trompie::HA.new
-ret = []
+files = []
 
+# cleanup
+FileUtils.rm_rf(TMPDIR)
+FileUtils.mkdir_p(TMPDIR)
+
+
+# get files from ha
 1.upto(STEPS) do |step|
   file = File.join(TMPDIR, "file-#{"%02i" % step}-#{Time.now.strftime("%Y%d%m-%H%M%S")}.jpg")
   fc = ha.make_req(:camera_proxy, "camera.livingroom_sw", raw: true)
   File.open(file, "w+") { |fp| fp.write(fc) }
-  ret << file
+  files << file
   sleep INTERVAL if STEPS != step
 end
 
-PROMPT = File.readlines(File.join(BASEDIR, "prompt.txt")).join
-
-llmargs = ["prompt", "\"#{PROMPT}\"", "-m", "llama-server-vision"]
-arguments = ret.map{ |f| ["-a", f] }
+# send files with prompt to llm
+llmargs = ["prompt", "\"#{prompt}\"", "-m", "llama-server-vision"]
+arguments = files.map{ |f| ["-a", f] }
 
 stdout_str, status = Open3.capture2("llm", *(llmargs + arguments).flatten)
 
-File.open(File.join(TMPDIR, "gemma-evaluation.txt"), "w+" ) { |fp| fp.puts(stdout_str) }
 
-combined = `nix-shell -p imagemagick --run 'cd #{BASEDIR} && ./report.sh'`
+# generate report
+rep = Report.new(files, prompt, stdout_str.strip)
+rep.workdir = "/home/mit/camcontest/report-#{Time.now.strftime('%Y%m%d%H%M')}"
+rep.save
 
-puts combined, stdout_str
+puts rep.output_file, stdout_str
--- include.rb
+++ include.rb
@@ -16,3 +16,82 @@ TMPDIR = File.join(BASEDIR, "work")
 # start fresh
 FileUtils.rm_rf(TMPDIR)
 FileUtils.mkdir_p(TMPDIR)
+
+def prompt(pfile = "prompt.txt")
+  @prompt ||= File.readlines(File.join(BASEDIR, pfile)).join
+end
+
+class Report
+  def initialize(files, prompt, text, resizeX: 640, resizeY: 480)
+    @files, @prompt, @text, @resizeX, @resizeY = files, prompt, text, resizeX, resizeY
+  end
+
+  def output_file=(filename)
+    @filename = filename
+  end
+
+  def result=(str)
+    @result = str
+  end
+
+  def result
+    @text
+  end
+
+  def level
+    result&.split("|").first.strip.to_i
+  end
+  
+  def workdir=(wd)
+    @workdir = wd
+  end
+
+  def workdir
+    @workdir ||= File.dirname(File.expand_path(@files.first))
+  end
+  
+  def output_file(activity_level = 1, version = 0)
+    @output_file ||=
+      begin
+        mkfilename = -> (v = version) { 
+          File.join(workdir, "report--level-#{activity_level}-version-#{"%02d" % v}.jpg")
+        }
+        filename = mkfilename.call(version)
+        while File.exist?(filename)
+          version += 1
+          filename = mkfilename.call(version)
+        end
+        filename
+      end
+  end
+
+  def save
+    unless File.exist?(workdir)
+      FileUtils.cp_r(TMPDIR, workdir)
+    end
+    stdout_str, status = Open3.capture2("magick", *magick_arguments)
+
+  end
+
+  def copy_or_save
+  end
+
+  def magick_arguments
+    cmd = [
+      "-background", "black",
+      "-fill", "white",
+      "-stroke", "black",
+      "-strokewidth", "0",
+      "-font", "DejaVu-Sans",
+      "-pointsize", "24",
+      "-size", "#{@resizeX}x",
+      "-gravity", "Center",
+      "caption:#{result}",
+      *@files,
+      "-resize", "#{@resizeX}x#{@resizeY}",
+      "-append",
+      output_file
+    ]
+
+  end
+end
